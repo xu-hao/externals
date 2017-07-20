b228925b8dbe1c13292e61c89f4351d15c82998c
Hao Xu <xuhao@default-cloud-hostname.example.org>
2017-07-20 01:01:53 -0400

    set environment variables

--- build.py
+++ build.py
@@ -234,7 +234,7 @@ def build_package(target):
     if get_package_type() == 'osxpkg' and target in ['jansson','zeromq4-1']:
         myenv['LIBTOOLIZE'] = 'glibtoolize'
         log.debug('LIBTOOLIZE='+myenv['LIBTOOLIZE'])
-    myenv['PATH'] = "~/.cabal/bin:{0}".format(myenv['PATH']) 
+    myenv['PATH'] = "{0}:{1}".format("~/.cabal/bin", myenv['PATH'])
 
     # build
     if target == 'clang':
